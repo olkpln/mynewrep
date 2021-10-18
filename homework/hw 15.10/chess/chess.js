408d3fff4937e3a38a6c213605e07f99d95c4dab
olkpln <olkpln@gmail.com>
2021-10-18 04:01:17 +0300

    homework

---
+++ homework/hw 15.10/chess/chess.js
@@ -0,0 +1,70 @@
+﻿$(function () {
+    $("td").click(whoIs);
+  });
+  
+  function whoIs(e) {
+    let hlpstr = "";
+    hlpstr += "This is " + e.target.id + ". ";
+    let hlpcls = e.target.classList;
+    if (!hlpcls.contains("white") && !hlpcls.contains("black")) {
+      hlpstr += "Empty here.";
+    } else {
+      if (hlpcls.contains("white")) {
+        hlpstr += "White ";
+      } else {
+        hlpstr += "Black ";
+      }
+      if (hlpcls.contains("king")) {
+        hlpstr += "king here.";
+      } else if (hlpcls.contains("queen")) {
+        hlpstr += "queen here.";
+      } else if (hlpcls.contains("rook")) {
+        hlpstr += "rook here.";
+      } else if (hlpcls.contains("bishop")) {
+        hlpstr += "bishop here.";
+      } else if (hlpcls.contains("knight")) {
+        hlpstr += "knight here.";
+      } else {
+        hlpstr += "pawn here.";
+      }
+    }
+    console.log(hlpstr);
+  }
+  
+  $(function () {
+    $("td").click(markStep);
+  });
+
+  function markStep(e) { 
+    // По клику на клетке пометить ее классом stepfrom.
+  
+    $(this).toggleClass("stepfrom");
+
+    // Если на кликнутой клетке уже есть такой класс, просто убрать его.
+    if ($("td").not(this).hasClass("stepfrom")) {
+        $(this).removeClass("stepfrom");
+    }
+    // Если есть другая клетка с этим классом, пометить кликнутую клетку классом stepto.
+
+    if ($("td").not(this).hasClass("stepfrom")) {
+      $(this).toggleClass("stepto");
+    }
+    // Если на кликнутой клетке есть класс stepto, просто убрать его.
+    if ($("td").not(this).hasClass("stepto")) {
+        $(this).removeClass("stepto");
+    }
+    // Если на доске уже есть другая клетка с классом stepto, убрать классы stepfrom и stepto с ранее помеченных клеток, 
+    if ($("td").not(this).hasClass("stepto")) {
+        $(this).removeClass("stepto");
+        $(this).removeClass("stepfrom");
+        //кликнутую пометить классом stepfrom.
+        //$(this).toggleClass("stepfrom");
+    }
+      // Если после всех манипуляций на доске остались клетки с классами stepfrom и steptoб вывести в консоль объект вида {stepfrom: <cell id>, stepto: <cell id>}
+    if ($("td").hasClass("stepfrom") && $("td").hasClass("stepto")) {
+        let x;
+        x = $(".stepfrom").attr("id");
+        console.log("stepfrom:" + x);
+        console.log("stepto:" + this.id);
+    };
+}
---
+++ homework/hw 15.10/footonjq/main.js
@@ -0,0 +1,62 @@
+﻿$(function(){
+    var field = $('#green').getBoundingClientRect();
+    var step = 10;
+    var borders = {
+        left: 0,
+        top: 0,
+        // вычисляем правый и нижний края
+        right: field.width,
+        bottom: field.height,
+    }
+    $('#red').on('mouseover', function(e){
+        var red = $('red')[0];
+        var coords = e.target.getBoundingClientRect();
+        // вычисляем центр красного блока
+        var center = {
+            x: coords.left + coords.width / 2,
+            y: coords.top + coords.height / 2,
+        }
+        // сравниваем координаты центра и координаты курсора мыши
+        // определяем в каком направлении двигаться
+        var delta = {
+            x: Math.sign(center.x - e.x),
+            y: Math.sign(center.y - e.y),
+        }
+        // определяем положение верхнего левого угла блока относительно его родителя
+        var oldplace = {
+            left: red.offsetLeft,
+            top: red.offsetTop,
+        }
+        // определяем положение верхнего левого угла блока относительно его родителя и вычисляем новое положение
+        var newplace = {
+            left: oldplace.left + step * delta.x,
+            top: oldplace.top + step * delta.y,
+        }
+        // проверяем нет ли касания границ поля.
+        if (borders.left > newplace.left) {
+            newplace.left = borders.left;
+        } else if (borders.right - coords.width < newplace.left) {
+            newplace.left = borders.right - coords.width - 2;
+        }
+        if (borders.top > newplace.top) {
+            newplace.top = borders.top;
+        } else if (borders.bottom - coords.height < newplace.top) {
+            newplace.top = borders.bottom - coords.height - 2;
+        }
+        // прописываем в стили красного блока новое положение
+        $('#red').css('left', (oldplace.left) + 'px');
+        $('#red').css('top', (red.offsetTop) + 'px');
+        var xstep = (newplace.left - oldplace.left) / 1000;
+        var ystep = (newplace.top - oldplace.top) / 1000;
+        for (var i = 1; i < 1001; i++) {
+            $('#red').css('left', (oldplace.left + xstep * i) + 'px');
+            $('#red').css('top', (oldplace.top + ystep * i) + 'px');
+            if ((i > 10) && (i < 990)) {
+                i += 9;
+                if ((i > 100) && (i < 900)) {
+                    i += 90;
+                }
+            }
+        }
+    });
+});
